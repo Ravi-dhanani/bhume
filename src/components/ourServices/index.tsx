1fec60b2ddb3c7263fce16810e0a47d10abbbece
Ravi-dhanani <117377500+Ravi-dhanani@users.noreply.github.com>
2024-07-09 12:05:42 +0530

    ui responsive testing

--- src/components/ourServices/index.tsx
+++ src/components/ourServices/index.tsx
@@ -209,7 +209,7 @@ const Services = () => {
             data-aos-anchor-placement="top-center"
             className="flex flex-col  items-center justify-between"
           >
-            <div className=" xl:gridxl:space-x-2 grid justify-center gap-y-2 sm:grid sm:grid-cols-1 sm:gap-2 md:grid md:grid-cols-2 md:space-y-0 lg:grid-cols-3  xl:grid-cols-3 2xl:grid-cols-4 2xl:space-x-2 ">
+            <div className=" xl:gridxl:space-x-2 grid justify-center gap-y-2 sm:grid sm:grid-cols-1 sm:gap-2 md:grid md:grid-cols-2 md:space-y-0 lg:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 2xl:space-x-2 ">
               {data
                 ?.filter((v: any) => v.language.title == id)
                 .map((item: any, index: number) => {
